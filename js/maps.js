7337b86bf9606ebd9b1c03e1bbc6f2080c8c6d29
Filip Stepien <contact@filipstepien.com>
2018-01-19 21:34:55 -0500

    create locations from json file instead of object

--- js/maps.js
+++ js/maps.js
@@ -9,29 +9,35 @@ function initMap() {
   });
 
 
+ 
+  // markers = [
+  //   {
+  //     coords: { lat: 43.6532, lng: -79.3831 },
+  //     iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+  //     content: '<h1>Home</h1>'
+  //   },
+  //   {
+  //     coords: { lat: 43.5890, lng: -79.6441 },
+  //     iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+  //     content: '<h1>Miss</h1>'
+  //   },
+  //   { coords: { lat: 44.5405, lng: -80.4081 } }
+  // ];
 
-  var markers = [
-    {
-      coords: { lat: 43.6532, lng: -79.3831 },
-      iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-      content: '<h1>Home</h1>'
-    },
-    {
-      coords: { lat: 43.5890, lng: -79.6441 },
-      iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-      content: '<h1>Miss</h1>'
-    },
-    { coords: { lat: 44.5405, lng: -80.4081 } }
-  ];
+// FETCH MARKERS FROM JSON
 
-
-
-  for (var i = 0; i < markers.length; i++) {
+fetch('js/locations.json')
+.then(res => res.json())
+.then((data) => {
+  markersData = data;
+  for (var i = 0; i < markersData.length; i++) {
     // Add marker
-    addMarker(markers[i]);
+    addMarker(markersData[i]);
   }
+})
+.catch((err) => {console.log(`Sorry, ${err}. Go to http://www.escapetorontonow.com/ for weekend ideas`)});
 
-  function addMarker(properties) {
+function addMarker(properties) {
 
     let marker = new google.maps.Marker({
       position: properties.coords,
@@ -46,7 +52,7 @@ function initMap() {
     }
     if (properties.content) {
       var infoWindow = new google.maps.InfoWindow({
-        content: properties.content
+        content: `${properties.content.location}`
       });
 
       marker.addListener('click', function () {
